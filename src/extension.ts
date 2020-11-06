b6c809d8b5e2624d47835cb55a2ad123719dccdf
Gaetan Chambouvet <gct2@3ds.com>
2020-11-06 20:23:42 +0100

    Code cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 import * as vscode from "vscode";
 import BolinetteChecker from "./utils/BolinetteChecker";
-import PythonCodeRunner from "./utils/AstGenerator";
+import AstGenerator from "./utils/AstGenerator";
 import BolinetteParser from "./core/BolinetteParser";
 
 export async function activate(context: vscode.ExtensionContext) {
-  PythonCodeRunner.init(context);
+  AstGenerator.init(context);
   const isBolinetteApp = await BolinetteChecker.isBolinetteApp();
   console.log(`isBolinetteApp: ${isBolinetteApp}`);
   if (isBolinetteApp) {
