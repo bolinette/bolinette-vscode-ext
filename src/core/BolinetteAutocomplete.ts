191b1b2b2b4f1575b67c784b8eb5d136fdac4330
Gaetan Chambouvet <gct2@3ds.com>
2020-11-12 19:55:43 +0100

    Refactor autocomplete

--- src/core/BolinetteAutocomplete.ts
+++ src/core/BolinetteAutocomplete.ts
@@ -4,8 +4,10 @@ import {
   ControllerParsedData,
   MixinParsedData,
   ModelParsedData,
+  ParsedData,
   ServiceParsedData,
 } from "parsed-data";
+import { parse } from "path";
 
 export class BolinetteAutocomplete {
   private project?: Project;
@@ -25,66 +27,26 @@ export class BolinetteAutocomplete {
             .lineAt(position)
             .text.substr(0, position.character);
 
-          switch (linePrefix) {
-            case "context.controller('":
-              const controllers = this.project?.getProjectFilesByType(
-                "controllers"
-              );
-              const controllerParsedData = controllers?.map((controller) =>
-                controller.getParsedData()
-              ) as ControllerParsedData[];
-              return (
-                controllerParsedData?.map(
-                  (el) =>
-                    new vscode.CompletionItem(
-                      el.classDefAnnotation?.getFirstParameter() || "",
-                      vscode.CompletionItemKind.Field
-                    )
-                ) || []
-              );
-            case "context.mixin('":
-              const mixins = this.project?.getProjectFilesByType("mixins");
-              const mixinParsedData = mixins?.map((service) =>
-                service.getParsedData()
-              ) as MixinParsedData[];
-              return (
-                mixinParsedData?.map(
-                  (el) =>
-                    new vscode.CompletionItem(
-                      el.classDefAnnotation?.getFirstParameter() || "",
-                      vscode.CompletionItemKind.Field
-                    )
-                ) || []
-              );
-            case "context.model('":
-              const models = this.project?.getProjectFilesByType("models");
-              const modelParsedData = models?.map((service) =>
-                service.getParsedData()
-              ) as ModelParsedData[];
-              return (
-                modelParsedData?.map(
-                  (el) =>
-                    new vscode.CompletionItem(
-                      el.classDefAnnotation?.getFirstParameter() || "",
-                      vscode.CompletionItemKind.Field
-                    )
-                ) || []
-              );
-            case "context.service('":
-              const services = this.project?.getProjectFilesByType("services");
-              const serviceParsedData = services?.map((service) =>
-                service.getParsedData()
-              ) as ServiceParsedData[];
-              return (
-                serviceParsedData?.map(
-                  (el) =>
-                    new vscode.CompletionItem(
-                      el.classDefAnnotation?.getFirstParameter() || "",
-                      vscode.CompletionItemKind.Field
-                    )
-                ) || []
-              );
+          const contextRegex = /context\.(?<type>(controller|mixin|model|service))\('/.exec(
+            linePrefix
+          );
+          if (contextRegex) {
+            const type = `${contextRegex.groups?.type}s`;
+            const files = this.project?.getProjectFilesByType(type);
+            const parsedData = files?.map((file) =>
+              file.getParsedData()
+            ) as ParsedData[];
+            return (
+              parsedData?.map(
+                (el) =>
+                  new vscode.CompletionItem(
+                    el.classDefAnnotation?.getFirstParameter() || "",
+                    vscode.CompletionItemKind.Field
+                  )
+              ) || []
+            );
           }
+
           return [];
         },
       }
